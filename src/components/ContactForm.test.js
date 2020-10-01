57ed7670821fe3a75e9924c495bf13e76051ab36
Adam <ruffneradam@aol.com>
2020-10-01 18:39:43 -0400

    added a couple more tests and asserts

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,28 +1,46 @@
-import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
-import ContactForm from './ContactForm'
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ContactForm from "./ContactForm";
 
+describe("Testing ContactForm", () => {
+  test("contact form renders without errors", () => {
+    render(<ContactForm />);
+  });
 
-test('contact form renders without errors', () => {
-    render (<ContactForm/>);
-})
-
-test('user can fill out form', async () => {
+  test("user can fill out form", async () => {
     //Arrange
-    render(<ContactForm/>);
+    render(<ContactForm />);
 
     //Act
     const firstNameInput = screen.getByLabelText(/first Name/i);
     const lastNameInput = screen.getByLabelText(/last Name/i);
     const emailInput = screen.getByLabelText(/email/i);
     const messageInput = screen.getByLabelText(/message/i);
-    const button = screen.getByRole('button', {name: /submit/i});
+    const button = screen.getByRole("button", { name: /submit/i });
 
-    fireEvent.change(firstNameInput, {target:{name: 'firstName', value: 'Bob'}});
-    fireEvent.change(lastNameInput, {target:{name: 'lastName', value: 'Ruffner'}});
-    fireEvent.change(emailInput, {target:{name: 'email', value: 'bob@bob.com'}});
-    fireEvent.change(messageInput, {target: {name: 'message', value: 'hello'}});
+    const newFirstName = screen.findByText("Adam");
+    const newLastName = screen.findByText("Ruffner");
+    const newEmailInput = screen.findByText("adam@adam.com");
+    const newMessageInput = screen.findByText("hello");
+
+    fireEvent.change(firstNameInput, {
+      target: { name: "firstName", value: "Bob" },
+    });
+    fireEvent.change(lastNameInput, {
+      target: { name: "lastName", value: "Ruffner" },
+    });
+    fireEvent.change(emailInput, {
+      target: { name: "email", value: "bob@bob.com" },
+    });
+    fireEvent.change(messageInput, {
+      target: { name: "message", value: "hello" },
+    });
     fireEvent.click(button);
-    //Assert
 
-})
+    //Assert
+    expect(newFirstName).toBeTruthy();
+    expect(newLastName).toBeTruthy();
+    expect(newEmailInput).toBeTruthy();
+    expect(newMessageInput).toBeTruthy();
+  });
+});
